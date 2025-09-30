d22a24a5d19a533c38e8120a9fa1d262e86d89b3
Olga Ferubko <ferubko.olga@gmail.com>
2025-09-30 18:54:25 +0300

    correct import

--- src/db/models/product.js
+++ src/db/models/product.js
@@ -1,5 +1,5 @@
 import { model, Schema } from 'mongoose';
-import { CATEGORIES } from '../constants/categories.js';
+import { CATEGORIES } from '../../constants/categories.js';
 
 const productsSchema = new Schema(
   {
